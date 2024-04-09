5da511fcbc5e410e9637de3291beed0391121b7b
Sebastian Kouridakis <23098501+kouridakis@users.noreply.github.com>
2024-04-09 13:27:07 +0300

    Add recipe detail page.

--- flavorblueprints/recipes/urls.py
+++ flavorblueprints/recipes/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "recipes"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path(f"{app_name}/<int:recipe_id>/", views.recipe, name="recipe"),
 ]
--- flavorblueprints/recipes/views.py
+++ flavorblueprints/recipes/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Recipe, Photo
 
 # Create your views here.
 def index(request):
     return render(request, "recipes/index.html")
+
+def recipe(request, recipe_id):
+    try:
+        recipe = Recipe.objects.get(pk=recipe_id)
+    except Recipe.DoesNotExist:
+        raise Http404("Recipe does not exist")
+    
+    photos = Photo.objects.filter(recipe=recipe)
+    return render(request, "recipes/recipe.html", {
+        "recipe": recipe,
+        "photos": photos
+    })
