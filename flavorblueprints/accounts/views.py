9362eba1056ba836d180beb46d6d1b4a80c4fdf7
Sebastian Kouridakis <23098501+kouridakis@users.noreply.github.com>
2024-04-09 12:18:52 +0300

    Create login and register pages.

--- flavorblueprints/accounts/views.py
+++ flavorblueprints/accounts/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 # Create your views here.
 def register(request):
-    return render(request, "accounts/register.html")
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = RegisterForm()
+
+    return render(request, "accounts/register.html", {"form": form})
