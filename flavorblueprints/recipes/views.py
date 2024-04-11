ac964201a79fc50ef9bdd4abb30b268366c17f3c
Sebastian Kouridakis <23098501+kouridakis@users.noreply.github.com>
2024-04-11 21:15:07 +0300

    Update recipes:index and the related template.

--- flavorblueprints/recipes/views.py
+++ flavorblueprints/recipes/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 from django.http import Http404
 from .models import SubCategory, Recipe, Photo
 
+def get_recipes_with_photos(recipes):
+    recipes_with_photos: list[tuple[Recipe, Photo]] = []
+    for recipe in recipes:
+        photo = Photo.objects.filter(recipe=recipe)[0]
+        recipes_with_photos.append((recipe, photo))
+    return recipes_with_photos
+
 # Create your views here.
 def index(request):
-    return render(request, "recipes/index.html")
+    # Get 10 most recent recipes
+    recipes = Recipe.objects.order_by("-timestamp")
+    if len(recipes) > 10:
+        recipes = recipes[:10]
+
+    recipes_with_photos = get_recipes_with_photos(recipes)
+
+    return render(request, "recipes/index.html", {
+        "recipes_with_photos": recipes_with_photos
+    })
 
 def recipe(request, recipe_id):
     try:
@@ -25,11 +41,8 @@ def category(request, category_id):
         raise Http404("Category does not exist")
     
     recipes = Recipe.objects.filter(category=category)
-    recipes_with_photos: list[tuple[Recipe, Photo]] = []
+    recipes_with_photos = get_recipes_with_photos(recipes)
 
-    for recipe in recipes:
-        photo = Photo.objects.filter(recipe=recipe)[0]
-        recipes_with_photos.append((recipe, photo))
     return render(request, "recipes/category.html", {
         "category": category,
         "recipes_with_photos": recipes_with_photos
