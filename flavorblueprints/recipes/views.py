543407e3da8de51938d82adcb252f5b38db88634
Sebastian Kouridakis <23098501+kouridakis@users.noreply.github.com>
2024-04-11 23:25:23 +0300

    Move get_recipes_with_photos to recipes.utilities.py.

--- flavorblueprints/recipes/views.py
+++ flavorblueprints/recipes/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import SubCategory, Recipe, Photo
-
-def get_recipes_with_photos(recipes):
-    """
-    Takes a list of Recipes, and returns a list of tuples associating
-    them to their first related Photo.
-
-    The returned tuples have the form (Recipe, Photo).
-    """
-    recipes_with_photos: list[tuple[Recipe, Photo]] = []
-    for recipe in recipes:
-        photo = Photo.objects.filter(recipe=recipe)[0]
-        recipes_with_photos.append((recipe, photo))
-    return recipes_with_photos
+from .utilities import get_recipes_with_photos
 
 # Create your views here.
 def index(request):
