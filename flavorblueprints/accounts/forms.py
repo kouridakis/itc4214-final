83d05fff44683e073b207ad7168de167ff57f069
Sebastian Kouridakis <23098501+kouridakis@users.noreply.github.com>
2024-04-12 23:32:21 +0300

    Update accounts.forms.EditForm to accept username changes.

--- flavorblueprints/accounts/forms.py
+++ flavorblueprints/accounts/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class EditForm(forms.Form):
+    username = forms.CharField(max_length=150) # Default max length for users is 150
     first_name = forms.CharField(max_length=255)
     last_name = forms.CharField(max_length=255)
     email = forms.EmailField()
@@ -10,11 +11,13 @@ class EditForm(forms.Form):
     def __init__(self, *args, **kwargs):
         user = kwargs.pop("user")
         super(EditForm, self).__init__(*args, **kwargs)
+        self.fields["username"].initial = user.username
         self.fields["first_name"].initial = user.first_name
         self.fields["last_name"].initial = user.last_name
         self.fields["email"].initial = user.email
 
     def save(self, user):
+        user.username = self.cleaned_data["username"]
         user.first_name = self.cleaned_data["first_name"]
         user.last_name = self.cleaned_data["last_name"]
         user.email = self.cleaned_data["email"]
