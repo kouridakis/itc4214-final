382b56052335619b7fbe349b54623f71adb70f52
Sebastian Kouridakis <23098501+kouridakis@users.noreply.github.com>
2024-04-12 23:31:44 +0300

    Register the Star table to Django's admin panel.

--- flavorblueprints/recipes/admin.py
+++ flavorblueprints/recipes/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import PrimaryCategory, SubCategory, Recipe, Photo
+from .models import PrimaryCategory, SubCategory, Recipe, Photo, Star
 
 # Register your models here.
 admin.site.register(PrimaryCategory)
 admin.site.register(SubCategory)
 admin.site.register(Recipe)
 admin.site.register(Photo)
+admin.site.register(Star)
