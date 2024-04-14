4829022c306da776bf08051481607572a72918c3
Sebastian Kouridakis <23098501+kouridakis@users.noreply.github.com>
2024-04-14 20:23:06 +0300

    Use regex in string.replace() to match all spaces, instead of just the first one.

--- flavorblueprints/search/static/search/js/search.js
+++ flavorblueprints/search/static/search/js/search.js
@@ -3,7 +3,7 @@ $(() => {
         // Prevent the form from submitting
         e.preventDefault();
         // Format the string as expected by the serverside view.
-        const query = $('#search-input').val().replace(' ', '+');
+        const query = $('#search-input').val().replace(/ /g, '+');
         // Redirect with the query in the URL.
         window.location.href = `/search/${encodeURIComponent(query)}`;
     });
